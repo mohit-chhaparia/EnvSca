846b617811a7f83253c1551f3cc529526e53a372
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-10-28 11:19:28 -0500

    Added the third function beta_classifier(). Description for each tag as per roxygen2 format will be added later.

---
+++ R/beta_classifier.R
@@ -0,0 +1,39 @@
+#'
+#' Classification using ONLY optimal scaling
+#'
+#' @param yt
+#' @param group
+#' @param L
+#' @param yt_new
+#' @return classes
+#' @examples
+#' To be added later
+#' @export
+beta_classifier <- function(yt, group, L, yt_new){
+  nnew <- dim(yt_new)[3]
+  if(is.na(nnew)){
+    nnew <- 1
+  }else{
+    nnew <- nnew
+  }
+  nclass <- length(unique(group))
+  classes <- c()
+  dist <- list()
+  for (j in 1:nclass){
+    dist[[j]] <- group_env(yt[ , , group == j] , L)$scale
+  }
+  for (k in 1:nnew){
+    if(nnew == 1){
+      new_dist <- env.get(yt_new , L)$scale
+    }else{
+      new_dist <- env.get(yt_new[ , , k] , L)$scale
+    }
+    g <- c()
+    for (j in 1:nclass){
+      temp <- dist[[j]]
+      g[j] <- sum((new_dist - temp) ^ 2)
+    }
+    classes[k] <- which(g == min(g))
+  }
+  return(classes)
+}
