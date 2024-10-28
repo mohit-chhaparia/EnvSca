7698843b12e6913b91460fc8f9f52fb781e20826
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-10-28 11:05:25 -0500

    Added the first function env_classifier(). Description for each tag as per roxygen2 format will be added later.

---
+++ R/env_classifier.R
@@ -0,0 +1,50 @@
+#'
+#' This is the main function: It assigns classes to a time series
+#'
+#' @param yt
+#' @param group
+#' @param L
+#' @param yt_new
+#' @param kappa
+#' @return classes
+#' @examples
+#' To be added later
+#' @export
+env_classifier <- function(yt, group, L, yt_new, kappa){
+  nnew = dim(yt_new)[3]
+  if(is.na(nnew)){
+    nnew <- 1
+  } else{
+    nnew <- nnew
+  }
+  nclass <- length(unique(group))
+  classes <- c()
+  env <- list()
+  scal <- list()
+  # calculate group level statistics based on training time series
+  for (j in 1:nclass){
+    env[[j]] <- group_env(yt[ , , group == j] , L)$envelope
+    scal[[j]] <- group_env(yt[ , , group == j] , L)$scale
+  }
+  # for each of testing time series, assign a group to it
+  for (k in 1:nnew){
+    if(nnew == 1){
+      new_env <- env.get(yt_new , L)$envelope
+      new_scal <- env.get(yt_new , L)$scale
+    }else{
+      new_env <- env.get(yt_new[ , , k] , L)$envelope
+      new_scal <- env.get(yt_new[ , , k] , L)$scale
+    }
+    g1 <- new_env
+    g2 <- new_scal
+    g <- c()
+    for (j in 1:nclass){
+      temp1 <- env[[j]]
+      temp2 <- scal[[j]]
+      g[j] <- kappa * sum((new_env - temp1) ^ 2) / sum(new_env ^ 2) +
+        (1 - kappa) * sum((new_scal - temp2) ^ 2) / sum(new_scal ^ 2)
+    }
+    classes[k] <- which(g == min(g))
+  }
+  return(classes)
+}
