e2f8c37a367c2d4caddc1109db83057697c6555e
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-10 09:41:16 -0600

    Resolved a global/local variable error.

--- src/group_env_c.cpp
+++ src/group_env_c.cpp
@@ -14,10 +14,11 @@ Rcpp::List group_env_c(const arma::cube& yt_group, int L) {
   int nsub = yt_group.n_slices;
   arma::vec specenv;
   double beta = 0.0;
+  Rcpp::List output;
 
   for (int k = 0; k < nsub; k++) {
 
-    Rcpp::List output = env.get(yt_group.slice(k), L);
+    output = env.get(yt_group.slice(k), L);
     arma::vec tmp_env = output["envelope"];
 
     if (k == 0) {
