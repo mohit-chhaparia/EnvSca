8a35b5021ab953641bdbd9d76968a9739d0e0fd0
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-10 00:15:31 -0600

    Added roxygen structure for group_env.

--- R/group_env.R
+++ R/group_env.R
@@ -1,3 +1,15 @@
+#'
+#' Given a several time series within a group
+#' Compute
+#' (1) the group level spectral envelope
+#' (2) the group level optimal scaling
+#'
+#' @param yt_group
+#' @param L
+#' @return
+#' @examples
+#' To be added later
+#' @export
 group_env <- function(yt_group, L){
   return(.Call(`_EnvSca_group_env_c`, yt_group, L))
 }
