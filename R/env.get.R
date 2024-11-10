f1ffa677a4505ad65ee52ef16660eb531896c328
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-09 23:47:29 -0600

    Added the roxygen structure for the env.get function.

--- R/env.get.R
+++ R/env.get.R
@@ -1,3 +1,15 @@
+#'
+#' Given a m times p-1 categorical time series, compute:
+#' (1) the Fourier frequencies
+#' (2) the spectral envelope
+#' (3) the optimal scaling
+#'
+#' @param yt
+#' @param L
+#' @return
+#' @examples
+#' To be added later
+#' @export
 env.get <- function(yt,L){
   fyy <- mvspec(yt, spans = c(L , L), plot = FALSE, kernel = "fejer")
   return(.Call(`_EnvSca_env.get_c`, yt, fyy))
