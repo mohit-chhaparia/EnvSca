1020ec474179cc8964def6b1abbb5c6c5faa5a70
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-10 02:07:58 -0600

    Added variable declaration and initialization using spectral density matrix, frequency, and number of observations used from fyy.

--- src/env.get_c.cpp
+++ src/env.get_c.cpp
@@ -12,6 +12,11 @@ Rcpp::List env.get_c(const arma::mat& yt, Rcpp::List fyy) {
   int dimen = yt.n_cols;
   arma::mat v = arma::cov(yt);
 
+  // Extract real part of spectral density matrix and frequency from fyy
+  arma::cube fyy_re = Rcpp::as<arma::cube>(fyy["fxx"]);  // Assuming fyy$fxx is a cube
+  arma::vec freq = Rcpp::as<arma::vec>(fyy["freq"]);
+  int num = Rcpp::as<int>(fyy["n.used"]); // Number of observations used
+
   return Rcpp::List::create(Rcpp::Named("freq") = freq,
                             Rcpp::Named("envelope") = specenv,
                             Rcpp::Named("scale") = beta);
