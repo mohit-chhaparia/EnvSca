0c5fc9c83f8dbf22064b62ad46558a446ac7b624
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-10 01:29:36 -0600

    Corrected file and function name.

--- src/env.get_c.cpp
+++ src/env.get_c.cpp
@@ -3,7 +3,7 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-Rcpp::List env_get_c(const arma::mat& yt, Rcpp::List fyy) {
+Rcpp::List env.get_c(const arma::mat& yt, Rcpp::List fyy) {
 
   return Rcpp::List::create(Rcpp::Named("freq") = freq,
                             Rcpp::Named("envelope") = specenv,
