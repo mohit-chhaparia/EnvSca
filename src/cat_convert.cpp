5c57910cbe248c0ef70544188def5ba401a63bdd
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-14 12:30:29 -0600

    Updated cat_convert_c function to handle string input.

--- src/cat_convert.cpp
+++ src/cat_convert.cpp
@@ -3,36 +3,24 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-arma::mat cat_convert_c(const arma::vec& xt) {
+arma::mat cat_convert_c(const std::vector<std::string>& xt) {
+  // Get unique values from xt
+  std::vector<std::string> stage = xt;
 
-  // Initialize a container for valid (finite) elements
-  arma::vec valid_xt;
+  std::sort(stage.begin(), stage.end());
+  Rcpp::Rcout << std::endl;
+  stage.erase(std::unique(stage.begin(), stage.end()), stage.end());
 
-  if (!arma::find_nonfinite(xt).is_empty()) {
-    std::vector<double> valid_values;
-    // Loop over xt to collect finite values
-    for (int i = 0; i < xt.n_elem; i++) {
-      if (arma::is_finite(xt(i))) {  // Check if the value is finite
-        valid_values.push_back(xt(i));
+  int nobs = xt.size();
+  int nstage = stage.size();
+  arma::mat yt(nobs, nstage - 1, arma::fill::zeros);
+
+  for (int j = 0; j < nstage - 1; ++j) {
+    for (int i = 0; i < nobs; ++i) {
+      if (xt[i] == stage[j]) {
+        yt(i, j) = 1;
       }
     }
-    // Convert valid_values back to an Armadillo vector
-    valid_xt = arma::vec(valid_values);
-  } else {
-    // If all elements are finite, use xt directly
-    valid_xt = xt;
   }
-
-  // Find unique stages (excluding NaN) and sort them
-  arma::vec stage = arma::sort(arma::unique(valid_xt));
-
-  int nobs = xt.n_elem;
-  int n_stage = stage.n_elem - 1;
-  arma::mat yt(nobs, n_stage, arma::fill::zeros);
-
-  for (int j = 0; j < n_stage; j++) {
-    yt.col(j) = arma::conv_to<arma::vec>::from(xt == stage(j));
-  }
-
   return yt;
 }
