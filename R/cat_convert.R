8781af79992f5c25219ad39a85ce7ac94a4d1d88
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-14 16:25:42 -0600

    Added conditions in cat_convert to convert xt to a character vector if it contains NA or is numeric.

--- R/cat_convert.R
+++ R/cat_convert.R
@@ -4,9 +4,11 @@
 #' @param xt DESCRIPTION
 #' @return DESCRIPTION
 #' @examples
-#' To be added later
+#' # To be added later
 #' @export
 cat_convert <- function(xt){
   if(length(xt) <= 0) stop("Length of xt is zero!!")
-  .Call(`_EnvSca_cat_convert_c`, xt)
+
+  if(any(is.na(xt)) | any(is.numeric(xt))) xt <- as.character(ifelse(is.na(xt), 'NA', xt))
+  return(.Call(`_EnvSca_cat_convert_c`, xt))
 }
