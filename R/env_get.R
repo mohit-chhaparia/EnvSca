ff2040dd12ab715e96ee29d23dc22639f234c6d0
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-14 18:01:50 -0600

    Added step to extract real part of fxx from mvspec() output before passing it to the C++ function.

--- R/env_get.R
+++ R/env_get.R
@@ -4,14 +4,15 @@
 #' (2) the spectral envelope
 #' (3) the optimal scaling
 #'
-#' @param yt DESCRIPTION
-#' @param L DESCRIPTION
+#' @param yt univariate or multivariate time-series
+#' @param L Integer giving the widths of modified Daniell smoothers to be used to smooth the periodogram
 #' @return DESCRIPTION
 #' @examples
-#' To be added later
+#' # To be added later
 #' @export
 #' @importFrom astsa mvspec
 env_get <- function(yt,L){
   fyy <- mvspec(yt, spans = c(L , L), plot = FALSE, kernel = "fejer")
+  fyy$fxx <- Re(fyy$fxx)
   return(.Call(`_EnvSca_env_get_c`, yt, fyy))
 }
