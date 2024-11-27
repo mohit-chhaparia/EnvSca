ebfae628f78b7b06d6a33aeb6243331a104cb6e9
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-27 01:17:25 -0600

    Added checks to verify user inputs to the env_get function.

--- R/env_get.R
+++ R/env_get.R
@@ -29,6 +29,29 @@
 #' @export
 #' @importFrom astsa mvspec
 env_get <- function(yt,L){
+
+  # Check if yt is NULL
+  if(is.null(yt)) stop("yt cannot be NULL")
+  # Check if yt is a matrix
+  if(!is.matrix(yt)) stop("yt must be a matrix")
+  # Check if yt has atleast two rows and one column
+  if(nrow(yt) < 2 | ncol(yt) < 1) stop("yt must have atleast 2 rows and 1 column.")
+  # Check if all elements in yt are numeric
+  if(!all(is.numeric(yt))) stop("All elements of yt must be numeric.")
+  # Check if yt is very large
+  if(ncol(yt) > 1e3) stop("yt has too many columns. Please reduce the dimensionality for computational feasibility.")
+
+  # Check if L is NULL
+  if(is.null(L)) stop("L cannot be NULL.")
+  # Check if L is numeric
+  if(!all(is.numeric(L))) stop("All elements of L should be positive integers.")
+  # Check if L falls in the right range
+  if(any(L <= 1) | any(L >= (nrow(yt) / 2)) | any(L != round(L)))
+    stop("L should be a integer between 1 and half of the number of rows in yt.")
+  if(any(L > (nrow(yt) ^ (1 / 3))))
+    warning("It is feasible to have L less than the cube root of the number of rows in yt")
+
+
   fyy <- mvspec(yt, spans = c(L , L), plot = FALSE, kernel = "fejer")
   fyy$fxx <- Re(fyy$fxx)
   return(.Call(`_EnvSca_env_get_c`, yt, fyy))
