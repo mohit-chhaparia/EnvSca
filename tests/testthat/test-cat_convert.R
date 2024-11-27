884c140bbd75a21c8508a8aaac01a54e6d6acdd4
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-27 00:17:17 -0600

    Added supressWarnings() in tests wherever we've manually instructed the function to throw warnings for inputs containing NAs.

--- tests/testthat/test-cat_convert.R
+++ tests/testthat/test-cat_convert.R
@@ -28,7 +28,8 @@ test_that("Test 4: cat_convert works with non-integer categories", {
 
 test_that("Test 5: cat_convert handles missing values", {
   xt <- c(1, 2, NA, 1, 3, NA)
-  expect_equal(cat_convert(xt), matrix(c(1, 0, 0, 1, 0, 0,
+  expect_warning(cat_convert(xt))
+  expect_equal(suppressWarnings(cat_convert(xt)), matrix(c(1, 0, 0, 1, 0, 0,
                                          0, 1, 0, 0, 0, 0,
                                          0, 0, 0, 0, 1, 0),
                                        nrow = 6, byrow = FALSE
@@ -38,7 +39,8 @@ test_that("Test 5: cat_convert handles missing values", {
 
 test_that("Test 6: cat_convert works with xt containing different data types including NAs.", {
   xt <- c(1, 2, 3, 2, 1, 3, 'a', 'b', NA, 'a', 'a', NA)
-  expect_equal(cat_convert(xt), matrix(c(1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  expect_warning(cat_convert(xt))
+  expect_equal(suppressWarnings(cat_convert(xt)), matrix(c(1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
                                          0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
                                          0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
                                          0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1,
@@ -50,6 +52,7 @@ test_that("Test 6: cat_convert works with xt containing different data types inc
 
 test_that("Test 7: cat_convert output dimensions depends on input xt.", {
   xt <- c(1, 2, 3, 2, 1, 3, 'a', 'b', NA, 'a', 'a', NA)
-  expect_equal(dim(cat_convert(xt)), c(length(xt), length(unique(xt)) - 1))
+  expect_warning(cat_convert(xt))
+  expect_equal(dim(suppressWarnings(cat_convert(xt))), c(length(xt), length(unique(xt)) - 1))
 })
 
