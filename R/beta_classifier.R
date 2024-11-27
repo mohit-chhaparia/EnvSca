4006540d3ad5036c7b48d1e20e475ba6c8142506
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-26 20:37:14 -0600

    Completed R documentation for beta_classifier function.

--- R/beta_classifier.R
+++ R/beta_classifier.R
@@ -1,13 +1,32 @@
 #'
-#' Classification using ONLY optimal scaling
+#' Classification Using only Optimal Scaling
 #'
-#' @param yt DESCRIPTION
-#' @param group DESCRIPTION
-#' @param L DESCRIPTION
-#' @param yt_new DESCRIPTION
-#' @return classes
+#' This function classifies test time-series based solely on the optimal scaling matrices derived from the
+#' training time-series groups.
+#'
+#' @param yt A 3D array representing the training time-series. Each slice corresponds to one time-series.
+#' @param group A vector of integers indicating the class/group for each time-series in \code{yt}. The
+#' length of \code{group} must match the number of slices in \code{yt}.
+#' @param L A integer or vector of integers giving the widths of modified Daniell smoothers to be
+#' used to smooth the periodogram. The value of the elements in \code{L} range from 1 to cube root of the number
+#' of rows of \code{yt}.
+#' @param yt_new A matrix representing the test time series. Dimensions are the same as \code{yt}.
+#' @return A numeric vector where each element corresponds to the predicted class label for a test time-series in
+#' \code{yt_new}.
 #' @examples
-#' To be added later
+#' set.seed(12092024)
+#'
+#' # Simulate training time-series for two classes
+#' yt <- array(rnorm(1500), dim = c(50, 3, 10))
+#' group <- c(rep(1, 5), rep(2, 5))
+#'
+#' # Simulate test time-series
+#' yt_new <- matrix(rnorm(150), nrow = 50)
+#'
+#' # Classify the test time-series
+#' classes <- beta_classifier(yt, group, L = 3, yt_new)
+#' print(classes)
+#'
 #' @export
 beta_classifier <- function(yt, group, L, yt_new){
   nnew <- dim(yt_new)[3]
