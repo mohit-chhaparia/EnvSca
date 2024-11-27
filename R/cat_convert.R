f8a13a60eb0cd383b6796c10a89c90d1298be47f
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-26 18:25:12 -0600

    Improved R documentation for cat_convert function.

--- R/cat_convert.R
+++ R/cat_convert.R
@@ -1,12 +1,25 @@
 #'
 #' Convert a categorical time-series into multivariate indicator process
 #'
-#' @param xt A vector containing the categorical time-series. The elements of this vector can be numeric, character, or NAs. If xt contains NAs then they will be treated as category 'NA'.
-#' @return Multivariate indicator matrix containing zeros and ones. Number of rows of yt is equal to the number of elements in xt. Number of columns of yt is to the number of unique elements in yt (including NAs) minus 1.
+#' This function transforms a categorical time series into a binary indicator matrix. Each category in the time
+#' series is represented as a column in the matrix, except for one reference category which is omitted.
+#'
+#' @param xt A vector containing the categorical time-series. The elements of this vector can be numeric,
+#' character, and can contain missing values (\code{NA}). If \code{xt} contains \code{NA} then they will
+#' be treated as a distinct category labeled 'NA'.
+#' @return A binary indicator matrix containing:
+#' \itemize{
+#'   \item \code{nrow}: Equal to the length of \code{xt}
+#'   \item \code{ncol}: Equal to the number of unique categories in \code{xt} (including 'NA') minus one.
+#'   The excluded category, used as the reference category, is the largest value in lexicographic order.
+#' }
+#'
 #' @examples
 #' xt <- c(1, 2, 3, 2, 1, 3, 'a', 'b', NA, 'a', 'a', NA)
 #' output <- cat_convert(xt)
 #' print(output)
+#' # Note: The reference category ('b' in this case) is excluded from the columns and is
+#' # represented by a row containing only zeros.
 #'
 #' @export
 cat_convert <- function(xt){
