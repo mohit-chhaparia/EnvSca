1859d4a8cdc9062add00e732ef589e764c3141ad
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-12-21 20:13:47 -0600

    Created test number 5 for env_get() function to test it with univariate time series.

--- tests/testthat/test-env_get.R
+++ tests/testthat/test-env_get.R
@@ -1,5 +1,4 @@
 test_that("Test 1: env_get works for simple input", {
-
   yt <- matrix(rnorm(20000), nrow = 500, ncol = 40)
   L <- c(3, 7, 11)
 
@@ -18,6 +17,7 @@ test_that("Test 1: env_get works for simple input", {
   expect_equal(dim(result$scale), c(250, 40))
 })
 
+
 test_that("Test 2: env_get handles single observation", {
   yt <- matrix(rnorm(4), nrow = 1, ncol = 4)
   L <- 2
@@ -30,6 +30,7 @@ test_that("Test 2: env_get handles single observation", {
   expect_equal(dim(result$scale), NULL)
 })
 
+
 test_that("Test 3: env_get works with small matrix", {
   yt <- matrix(rnorm(15), nrow = 5, ncol = 3)
   L <- 2
@@ -41,3 +42,23 @@ test_that("Test 3: env_get works with small matrix", {
   expect_length(result$envelope, 2)
   expect_equal(dim(result$scale), c(2, 3))
 })
+
+
+test_that("Test 4: env_get handles univariate time series", {
+  yt <- matrix(rnorm(500), nrow = 500, ncol = 1)
+  L <- 3
+
+  result <- env_get(yt, L)
+
+  expect_type(result, "list")
+  expect_named(result, c("freq", "envelope", "scale"))
+
+  expect_type(result$freq, "double")
+  expect_type(result$envelope, "double")
+  expect_type(result$scale, "double")
+
+  expect_length(result$freq, 250)
+  expect_length(result$envelope, 250)
+  expect_equal(dim(result$scale), c(250, 1))
+})
+
